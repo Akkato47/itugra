22feac1617a123ea6084e32780cd55adcee328a6
Sergey Yatsuk <v1s.coop@yandex.ru>
2024-11-13 13:48:55 +0500

    .

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,7 @@
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import "dotenv/config";
 import path from "path";
 import { defineConfig } from "vite";
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
