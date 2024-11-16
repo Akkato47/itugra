698faefba5724bd02d12037e22c210c903b8893e
kato <unknown.255.galactic.acc@gmail.com>
2024-11-16 18:20:40 +0500

    fix

--- back/src/db/drizzle/migrate.ts
+++ back/src/db/drizzle/migrate.ts
@@ -1,15 +1,18 @@
 import config from '../../config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import path from 'path';
 import postgres from 'postgres';
 
 const db_migrate = async () => {
   const migrationClient = postgres(config.database.postgres.url, { max: 1 });
 
+  const migrationsFolder = config.app.isProduction
+    ? path.join(__dirname, '../../../dist/db/drizzle/migrations')
+    : path.join(__dirname, '../../../src/db/drizzle/migrations');
+  console.log(migrationsFolder, config.app.isProduction);
   await migrate(drizzle(migrationClient), {
-    migrationsFolder: config.app.isProduction
-      ? './dist/db/drizzle/migrations'
-      : './src/db/drizzle/migrations',
+    migrationsFolder: migrationsFolder,
   });
 
   await migrationClient.end();
